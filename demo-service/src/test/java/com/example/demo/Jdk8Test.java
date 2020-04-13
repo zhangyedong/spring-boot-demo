6d725107dee050ce8e5ce4cd02f68cc039e0b286
zhangyedong <786757521@qq.com>
2020-04-14 00:00:07 +0800

    8

--- demo-service/src/test/java/com/example/demo/Jdk8Test.java
+++ demo-service/src/test/java/com/example/demo/Jdk8Test.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * jdk8
@@ -23,7 +24,19 @@ public class Jdk8Test {
 
     @Test
     public void test() {
+        List<User> userList = new ArrayList<>();
         File[] hiddenFiles = new File(".").listFiles(File::isHidden);
+        filterUsers(userList, Jdk8Test::isMoreTwo);
+        filterUsers(userList, Jdk8Test::isMoreThree);
+        filterUsers(userList, u -> u.getId() > 2 || u.getId() > 3);
+
+        userList.stream()
+                .filter(u -> u.getId() > 2)
+                .collect(Collectors.toList());
+        //并行
+        userList.parallelStream().filter(u -> u.getId() > 2).collect(Collectors.toList());
+
+
     }
 
     public static List<User> filterUsers(List<User> userList, Predicate<User> p) {
@@ -34,5 +47,13 @@ public class Jdk8Test {
             }
         }
         return result;
+    }
+
+    public static boolean isMoreTwo(User user) {
+        return user.getId() > 2;
+    }
+
+    public static boolean isMoreThree(User user) {
+        return user.getId() > 3;
     }
 }
