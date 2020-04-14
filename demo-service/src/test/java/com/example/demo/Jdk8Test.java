448034bae5f5bb4fc962df888212af22626cbb5f
zhangyedong <786757521@qq.com>
2020-04-14 18:21:59 +0800

    8

--- demo-service/src/test/java/com/example/demo/Jdk8Test.java
+++ demo-service/src/test/java/com/example/demo/Jdk8Test.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -36,14 +37,35 @@ public class Jdk8Test {
         //并行
         userList.parallelStream().filter(u -> u.getId() > 2).collect(Collectors.toList());
 
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
 
+            }
+        });
+        thread = new Thread(() -> System.out.println("111"));
+//        Optional<String> optional = Optional.empty();
+//        System.out.println("-----optional----" + optional);
+//        Optional.ofNullable(new User()).filter(u -> u.getId() > 2).ifPresent(u -> System.out.println(u.getName()));
+//        Optional<Long> num = Optional.ofNullable(new User()).map(User::getId);
+//        Long num1 = Optional.ofNullable(new User()).map(User::getId).orElse(0L);
+
+
+        Comparator<User> comparator = Comparator.comparing(User::getId);
+//        Predicate 断言
+//        Consumer 消费
+//        Function 功能
+//        Supplier 提供
+
+        //方法引用 lambda的语法糖，他的另外一种写法
+        userList.sort(Comparator.comparing(User::getId));
     }
 
-    public static List<User> filterUsers(List<User> userList, Predicate<User> p) {
-        List<User> result = new ArrayList<>();
-        for (User user : userList) {
-            if (p.test(user)) {
-                result.add(user);
+    public static <T> List<T> filterUsers(List<T> userList, Predicate<T> p) {
+        List<T> result = new ArrayList<>();
+        for (T t : userList) {
+            if (p.test(t)) {
+                result.add(t);
             }
         }
         return result;
